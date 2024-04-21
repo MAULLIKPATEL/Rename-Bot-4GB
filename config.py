413a9ef797e59b7b1a17af3cfae83b26bde08c5b
MAULLIKPATEL <119292297+MAULLIKPATEL@users.noreply.github.com>
2024-04-21 17:13:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 
 DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
+DB_NAME = os.environ.get("DB_NAME", "")
 
 STRING = os.environ.get("STRING", "")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
